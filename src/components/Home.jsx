bdc9c351743e7e9046ed4262691bd04c6a77bf8c
Michael Chi <michael.chi1997@gmail.com>
2018-12-30 14:14:56 -0500

    WIP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import "../styles/Home.scss";
 import Projects from "./Projects";
 import Work from "./Work";
 const ProjectsArray = require("../data/Projects.json").projects;
+const WorksArray = require("../data/Works.json").works;
 
 
 class Home extends Component {
@@ -10,13 +11,15 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            projects: []
+            projects: [],
+            works: []
         }
     }
 
     componentWillMount() {
         this.setState({
-            projects: ProjectsArray
+            projects: ProjectsArray,
+            works: WorksArray
         })
     }
 
@@ -24,10 +27,10 @@ class Home extends Component {
         return (
             <div>
                 <div className="container">
-                     <div className="intro">
-                         <h1>Hi, My name is Michael Chi</h1>
-                         <p>I'm a full-stack software developer</p>
-                     </div>
+                    <div className="intro">
+                        <h1>Hi, My name is Michael Chi</h1>
+                        <p>I'm a full-stack software developer</p>
+                    </div>
                 </div>
             </div>
         );
@@ -37,6 +40,19 @@ class Home extends Component {
         return (
             <div>
                 {this.homeSection()}
+                <div className="section-wrapper">
+                    <div className="section-title">
+                        <h1>Work Experience</h1>
+                    </div>
+                    <Work works={this.state.works} />
+                </div>
+                <div className="section-wrapper">
+                    <div className="section-title">
+                        <h1>Projects</h1>
+
+                    </div>
+                    <Projects projects={this.state.projects} />
+                </div>
             </div>
         );
     }
