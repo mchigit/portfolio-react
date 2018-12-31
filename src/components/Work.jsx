fb00f2ed5d71ce3b85d66aa3d05b8ac2253e3d68
Michael Chi <michael.chi1997@gmail.com>
2018-12-31 15:03:29 -0500

    Styled work experience

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,18 +2,32 @@ import React, { Component } from "react";
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import "../styles/Work.scss";
+import WorkIcon from '@material-ui/icons/Work';
 
 class Work extends Component {
+
+    icon = () => {
+        return <i class="material-icons">
+            work
+        </i>
+    }
 
     renderWorkExperience = (works) => {
         const workTimeline = works.map((work, key) => (
             <VerticalTimelineElement key={key}
-             className="vertical-timeline-element--work"
-              iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-              >
-                <h3 className="vertical-timeline-element-title">{work.title}</h3>
-                <h4 className="vertical-timeline-element-subtitle">{work.company}</h4>
-                <h4>{work.date}</h4>
+                iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                icon={<WorkIcon />}
+            >
+                <div className="work-summary">
+                    <div className="work-titles">
+                        <h3 className="vertical-timeline-element-title">{work.title}</h3>
+                        <h4 className="vertical-timeline-element-subtitle">{work.company}</h4>
+                        <h4 className="vertical-timeline-element-subtitle">{work.date}</h4>
+                    </div>
+                    <div className="work-logo">
+                        <img src={require(`../images/${work.logo}`)} alt="company-logo" />
+                    </div>
+                </div>
                 <p>
                     {work.description}
                 </p>
@@ -27,8 +41,8 @@ class Work extends Component {
         return (
             <div className="container work">
                 <div className="work-wrapper">
-                    <VerticalTimeline  className="work-timeline">
-                       {this.renderWorkExperience(this.props.works)}
+                    <VerticalTimeline>
+                        {this.renderWorkExperience(this.props.works)}
                     </VerticalTimeline>
                 </div>
             </div>
